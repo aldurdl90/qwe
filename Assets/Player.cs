30ac58ec5635fc78f312c7b15229bdac96bd82a2
aldur <rnrwoghks@gmail.com>
2025-07-15 16:22:01 +0900

    코딩 살짝 추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    void Start()
+    public float h;
+    public float jumpForce;
+    public float speed;
+    public float maxSpeed;
+    Rigidbody2D rb;
+    bool isGrounded = true; 
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        h= Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded )
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(h * speed, 0), ForceMode2D.Force);
+
+
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else if (collision.gameObject.CompareTag("Monster"))
+        {
+           
+        }
     }
 }
